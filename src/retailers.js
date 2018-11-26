5bf1d5c80d22a38145ea8b5510d521005dbe2173
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-11-26 03:13:09 +0000

    Re-try parts retrieved from element14 with octopart

--- src/retailers.js
+++ src/retailers.js
@@ -1,5 +1,7 @@
 const immutable = require('immutable')
 
+const octopart = require('./octopart')
+
 const retailers = {
   Farnell: require('./farnell'),
   //disabled for now until we can optimize
@@ -55,10 +57,23 @@ function run(part) {
   ).then(newOffers => {
     newOffers = immutable.List(newOffers).flatten(1)
     if (!part.get('mpn')) {
-      const withMpn = newOffers.find(x => x.get('mpn'))
-      if (withMpn) {
-        part = part.set('mpn', withMpn.get('mpn'))
+      //if octopart didn't find it the first time then get an mpn from the
+      //retailers and try that
+      const with_mpn = newOffers.find(x => x.get('mpn'))
+      if (with_mpn) {
+        const mpn = with_mpn.get('mpn')
+        const query = immutable.Map({mpn})
+        return octopart(immutable.List.of(query)).then(r => {
+          const octopart_part = r.get(query)
+          if (octopart_part) {
+            return octopart_part.update('offers', o => o.concat(newOffers))
+          }
+          return part
+            .set('offers', newOffers.concat(not_yet_offers))
+            .set('mpn', mpn)
+        })
       }
+      return part.set('offers', newOffers.concat(not_yet_offers))
     }
     return part.set('offers', newOffers.concat(not_yet_offers))
   })
