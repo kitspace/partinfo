0760330f37cae5de0228b6615237e1b6099ce0dc
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2019-10-19 15:31:46 +0100

    Start getting part info for lcsc

--- src/lcsc.js
+++ src/lcsc.js
@@ -10,14 +10,28 @@ const browserPromise = createHeadless()
 const runQuery = rateLimit(30, 1000, async function(term) {
   const browser = await browserPromise
   const url = 'https://lcsc.com/search?q=' + term
+  console.log({url})
   const page = await browser.newPage()
   await page.goto(url)
   await page.waitFor('.mfrPartItem')
-  const titles = await page.evaluate(() => {
-    const ts = document.querySelectorAll('.mfrPartItem')
-    return Array.from(ts).map(t => t.innerHTML)
+  const parts = await page.evaluate(() => {
+    let rows = document.querySelectorAll('.el-table__row')
+    rows = Array.from(rows)
+    // the rows seem to be copied 3 times, we discard a third
+    rows = rows.slice(0, rows.length / 3)
+    return rows.map(row => {
+      const part = row.querySelector('.mfrPartItem > a').innerText
+      const manufacturer = row.querySelector('.manufacturerItem > a').innerText
+      let in_stock_quantity = row.querySelector('.stockItem > a > .text-num')
+      if (in_stock_quantity == null) {
+        in_stock_quantity = 0
+      } else {
+        in_stock_quantity = parseInt(in_stock_quantity.innerText, 10)
+      }
+      return {part, manufacturer, in_stock_quantity}
+    })
   })
-  console.log(titles)
+  console.log(parts)
 })
 
 function lcsc(queries) {
@@ -29,7 +43,9 @@ function lcsc(queries) {
       if (term != null) {
         return runQuery(term)
       } else if (mpn != null) {
-        return runQuery(mpn.get('manufacturer') + ' ' + mpn.get('part'))
+        let q = mpn.get('manufacturer') + ' ' + mpn.get('part')
+        q = q.trim()
+        return runQuery(q)
       } else if (sku != null && sku.get('vendor') === 'LCSC') {
         return runQuery(sku.get('part'))
       }
